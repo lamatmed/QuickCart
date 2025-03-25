d8fd67d84ea23f588f1631e2a979b570e4572502
lamatmed <lamat032025@gmail.com>
2025-03-25 17:19:54 +0100

    Correction des warnings ESLint et optimisation useEffect

--- config/inngest.js
+++ config/inngest.js
@@ -3,7 +3,7 @@ import connectDB from "./db";
 import User from "@/models/User";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "QuickCart" });
+export const inngest = new Inngest({ id: "quickcart" });
 
 
 
